e26ab87b4b20c4f9b24d75e661d910b168272324
yusuShin <tlsdbtn0507@naver.com>
2022-11-03 20:55:08 +0900

    검색어 와 날짜 태그로 공간 검색 페이지
    공간 링크와 검색어, 날짜와 일치한 결과들 출력 및 공간 타입별 출력 세분화 완료

--- SpaceZFE/src/components/SelectAll.vue
+++ SpaceZFE/src/components/SelectAll.vue
@@ -1,24 +1,85 @@
 <template>
   <div>
-    <select name="spaceType" id="spaceType" v-model="spaceType">
+    <div class="filter">
+      검색어 결과 : {{ resultSplit[0] }} <br />
+      검색 날짜 : {{ resultSplit[1] }} - {{ resultSplit[2] }}
+    </div>
+    <select
+      class="spaceType"
+      id="spaceType"
+      v-model="spaceType"
+      @change="sortSpace"
+    >
       <option value="전체" selected>전체</option>
       <option value="오피스">오피스</option>
       <option value="데스크">데스크</option>
       <option value="회의실">회의실</option>
     </select>
+    <br />
+    <p class="placeLeng" v-show="!optionClicked">
+      <b>장소</b> : {{ places.length }}
+    </p>
+    <p class="placeLeng" v-show="optionClicked">
+      <b>장소</b> : {{ filteredPlace.length }}
+    </p>
+
+    <div v-show="!optionClicked" class="selectedAll">
+      <section
+        class="lists"
+        v-for="num in places.length"
+        :key="places[num]"
+        @click="moveToPage(places[num - 1].spaceId)"
+      >
+        <img
+          class="img"
+          :src="`http://localhost:8090/spaceZBE/resources/upload/${
+            places[num - 1].imgName
+          }`"
+        />
+        <h3>{{ places[num - 1].spaceName }}</h3>
+        <p>{{ places[num - 1].price }}원 {{ places[num - 1].type }}</p>
+        <p></p>
+      </section>
+    </div>
+    <div v-show="optionClicked" class="selectedAll">
+      <section
+        class="lists"
+        v-for="num in filteredPlace.length"
+        :key="filteredPlace[num]"
+        @click="moveToPage(filteredPlace[num - 1].spaceId)"
+      >
+        <img
+          class="img"
+          :src="`http://localhost:8090/spaceZBE/resources/upload/${
+            filteredPlace[num - 1].imgName
+          }`"
+        />
+        <h3>{{ filteredPlace[num - 1].spaceName }}</h3>
+        <p>
+          {{ filteredPlace[num - 1].price }}원 {{ filteredPlace[num - 1].type }}
+        </p>
+        <p></p>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
-import { axios } from "axios";
+import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
 import { ref } from "vue";
+import _ from "lodash";
+
 export default {
   setup() {
-    const places = ref([]);
+    const places = ref({});
+    const filteredPlace = ref({});
+    const router = useRouter();
     const route = useRoute();
+    const spaceType = ref("");
     const result = route.params.id;
     const resultSplit = result.split(" ");
+    const optionClicked = ref(false);
 
     const getPlaces = async () => {
       const res = await axios
@@ -26,14 +87,79 @@ export default {
           `http://localhost:8090/spaceZBE/search?searchWord=${resultSplit[0]}&searchTime=${resultSplit[1]} ${resultSplit[2]}`
         )
         .then((res) => {
-          places.value = { ...res.data.vos };
+          places.value = res.data.vos;
+          console.log(places.value);
         });
     };
 
-    console.log(resultSplit[0], resultSplit[1], resultSplit[2]);
-    return {};
+    getPlaces();
+
+    const sortSpace = () => {
+      optionClicked.value = true;
+      if (spaceType.value === "오피스") {
+        filteredPlace.value = _.filter(places.value, { type: "office" });
+        console.log(spaceType.value);
+      } else if (spaceType.value === "데스크") {
+        filteredPlace.value = _.filter(places.value, { type: "desk" });
+        console.log(spaceType.value);
+      } else if (spaceType.value === "회의실") {
+        filteredPlace.value = _.filter(places.value, { type: "meetingRoom" });
+        console.log(spaceType.value);
+      } else if (spaceType.value === "전체") {
+        filteredPlace.value = places.value;
+      }
+    };
+
+    const moveToPage = (placeId) => {
+      router.push({
+        name: "Details",
+        params: {
+          id: placeId,
+        },
+      });
+    };
+
+    return {
+      getPlaces,
+      places,
+      optionClicked,
+      spaceType,
+      resultSplit,
+      sortSpace,
+      filteredPlace,
+      moveToPage,
+    };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.filter {
+  font-size: 20px;
+}
+.selectedAll {
+  width: 1600px;
+  height: 450px;
+  margin: none;
+}
+.lists {
+  display: inline-block;
+  margin: 30px;
+}
+.img {
+  width: 300px;
+  height: 300px;
+}
+.spaceType {
+  margin-top: 10px;
+  font-size: 20px;
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  opacity: 0.6;
+}
+.placeLeng {
+  margin-top: 20px;
+  font-size: 20px;
+}
+</style>
